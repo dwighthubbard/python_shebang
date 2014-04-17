c1fc6a4795a73b232643680ff045e87d47db1d85
Dwight Hubbard <dhubbard@yahoo-inc.com>
2014-04-17 15:29:07 -0700

    Add the pycharm .idea directory to .gitignore

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+
+setup(
+    name='python_shebang',
+    version='0.0.1',
+    author='Dwight Hubbard',
+    author_email='d@dhub.me',
+    packages=['python_shebang'],
+    url='',
+    license='Apache V2',
+    keywords="shebang hashbang ",
+    description="Shebang tool to find a compatible python interpreter for a "
+                "script",
+    long_description=open('README.md').read(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: Apache License V2",
+    ],
+    scripts=['python_shebang/bin/python_shebang']
+)
