4aa4997d83fc2ff95f89400e97d987e2e5f6a8c6
Dwight Hubbard <dhubbard@yahoo-inc.com>
2015-11-10 12:59:15 -0800

    Code cleanups
    Don't fail on some import operations

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='python_shebang',
-    version='0.0.1',
+    version='0.0.2',
     author='Dwight Hubbard',
     author_email='d@dhub.me',
     packages=['python_shebang'],
